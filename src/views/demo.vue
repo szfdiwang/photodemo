0f84dedbd72048322f2fc883d33f0b17b20d19fd
wangchenchen <1726021@qq.com>
2019-08-30 01:23:42 +0800

    完善

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -171,38 +171,6 @@
         ]
       }
     },
-    directives: {
-      drag: {
-        bind: function (el) {
-          console.log(el)
-          let odiv = el; //获取当前元素
-          odiv.onmousedown = (e) => {
-            console.log(e)
-            //算出鼠标相对元素的位置
-            let disX = e.clientX - odiv.offsetLeft;
-            let disY = e.clientY - odiv.offsetTop;
-
-            document.onmousemove = (e) => {
-              //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
-              let left = e.clientX - disX;
-              let top = e.clientY - disY;
-
-              //绑定元素位置到positionX和positionY上面
-              this.positionX = top;
-              this.positionY = left;
-
-              //移动当前元素
-              odiv.style.left = left + 'px';
-              odiv.style.top = top + 'px';
-            };
-            document.onmouseup = (e) => {
-              document.onmousemove = null;
-              document.onmouseup = null;
-            };
-          };
-        }
-      }
-    },
     methods: {
       getIndex(arr, obj) {
         let len = arr.length;
